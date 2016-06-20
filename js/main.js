ffa34c179afa45bd04ca9f8817c3d7abe37ebdc7
dok <jorgecbll@gmail.com>
2016-06-20 18:48:41 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+});
+
+$(function(){
+        $(".element").typed({
+          strings: ["<dok>: Hello, friend.^2000\n<dok>: Welcome.^2000"+
+          "\n<dok>: I do: Frontend development^1000\n<dok>: Javascript. Vuejs. Cute apps.^2000"+
+          "\n<dok>: Currently: @ freecodecamp learning to code.^2000"],
+          typeSpeed: 20,
+          backDelay: 500,
+          loop: false,
+          loopCount: false,
+          contentType: 'text'
+        });
+    });
+
+$(function(){
+            $(".name").typed({
+              strings: ["Jorge Cabello.", "Jorge."],
+              typeSpeed: 20,
+              startDelay: 2000,
+              backDelay: 500,
+              loop: true,
+              loopCount: 3,
+              contentType: 'text'
+            });
+        });
+
+/*
+
+ I don't just hack you, Krista. I hack everyone, my friends, coworkers,
+ but I've helped a lot of people. I want a way out of loneliness,
+ just like you. Is that what you wanted to hear?
+
+ */
