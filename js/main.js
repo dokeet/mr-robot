ab457989577429b7cbe57c7dbd6cf4aaea7a0ec9
dok <jorgecbll@gmail.com>
2016-06-24 17:13:43 -0400

    add

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
       $('.parallax').parallax();
-
+      $(".button-collapse").sideNav();
 });
+
+
 
 $(function(){
         $(".element").typed({
@@ -18,9 +20,46 @@ $(function(){
         });
     });
 
+    // Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
 
 
-
- // I don't just hack you, Krista. I hack everyone, my friends, coworkers,
- // but I've helped a lot of people. I want a way out of loneliness,
- // just like you. Is that what you wanted to hear?
+ //I don't just hack you, Krista. I hack everyone, my friends, coworkers,
+ //but I've helped a lot of people. I want a way out of loneliness,
+ //just like you. Is that what you wanted to hear?
